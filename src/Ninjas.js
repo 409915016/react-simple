7337996551aecacad42a46b13c6c104b96aef1af
mather <misaka@misaka.im>
2020-08-21 19:00:08 +0800

    forms revisited

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -3,16 +3,16 @@ import React from 'react'
 const Ninjas = ({ninjas}) => {
     const ninjasList = ninjas.map(i => {
         return (
-          <div className="ninja" key={ i.id }>
+            <div className="ninja" key={ i.id }>
               <div>Name: { i.name }</div>
               <div>Age: { i.age }</div>
-          </div>
+            </div>
         )
     });
     return (
-      <div className="ninja-list">
+        <div className="ninja-list">
           { ninjasList }
-      </div>
+        </div>
     )
 }
 
