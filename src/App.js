34ed8d1c6e7d72ba2ec23d78fd0ad72b605c3c90
mather <misaka@misaka.im>
2020-08-29 19:25:19 +0800

    route switch

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './components/Home'
 import Contact from './components/Contact'
 import About from './components/About'
 import Post from './components/Post'
-import { BrowserRouter, Route } from "react-router-dom"
+import { BrowserRouter, Route, Switch } from "react-router-dom"
 
 class App extends Component {
   render() {
@@ -12,10 +12,12 @@ class App extends Component {
         <BrowserRouter>
             <div className="App">
                 <NavBar />
-                <Route exact path="/" component={Home}/>
-                <Route path="/about" component={About}/>
-                <Route path="/contact" component={Contact}/>
-                <Route path="/:post_id" component={Post}/>
+                <Switch>
+                  <Route exact path="/" component={Home}/>
+                  <Route path="/about" component={About}/>
+                  <Route path="/contact" component={Contact}/>
+                  <Route path="/:post_id" component={Post}/>
+                </Switch>
             </div>
         </BrowserRouter>
       
