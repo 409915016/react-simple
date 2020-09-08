45114e9432927c05c57d6d92563e80388173b254
mather <misaka@misaka.im>
2020-09-09 00:44:16 +0800

    updating context data

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Navbar from './components/Navbar'
 import Booklist from './components/BookList';
 import ThemeContextProvider from './contexts/ThemeContext';
+import ThemeToogle from './components/ThemeToggle';
+
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
         <Navbar />
         <Booklist />
+        <ThemeToogle />
       </ThemeContextProvider> 
     </div>
   );
