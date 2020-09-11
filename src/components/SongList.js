c5f83d0157bb1db096b792c1b0a2e91c96b19763
mather <misaka@misaka.im>
2020-09-11 14:58:18 +0800

    useEffect Hook

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,17 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {v1 as uuid} from 'uuid'
+import NewSongForm from './newSongForm';
 
 const SongList = () => {
-
 
   const [songs, setSongs] = useState([
     { title: 'almost home', id: 1},
     { title: 'memory gospe', id: 2},
     { title: 'this wild darkness', id: 3}
   ]);
+  const [age, setAge] = useState(20)
 
-  const addSong = () =>{
-    setSongs([...songs, {title: 'new song', id: uuid()}])
+  useEffect(() => {
+    console.log('useEffcet hook ran', songs)
+  }, [songs])
+
+  
+  useEffect(() => {
+    console.log('useEffcet hook ran', age)
+  }, [age])
+
+  const addSong = (title) =>{
+    setSongs([...songs, {title, id: uuid()}])
   }
   
   return (  
@@ -21,7 +31,8 @@ const SongList = () => {
           return (<li key={i.id}> { i.title }</li>)
         })}
       </ul>
-      <button onClick={addSong}>Add a song</button>
+      <NewSongForm addSong={ addSong }/>
+      <button onClick={ () => setAge(age +1 )}>Add 1 to age: {age}</button>
     </div>
   );
 }
