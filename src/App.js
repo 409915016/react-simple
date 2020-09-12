3f79c653d5e5c395a2338a08a30040f98ed5b7ca
mather <misaka@misaka.im>
2020-09-12 19:06:23 +0800

    remove some files

--- src/App.js
+++ src/App.js
@@ -1,49 +1,12 @@
 import React, {Component} from 'react'
-import Ninjas             from './Ninjas'
-import AddNinja           from './AddNinja'
-
 class App extends Component {
-    state = {
-        ninjas: [
-            {id: 1, name: 'Mather', age: 26},
-            {id: 2, name: 'Yoshi', age: 20}
-        ]
-    }
-    addNinja = (ninja) => {
-        ninja.id = Math.random()
-        let ninjas = [...this.state.ninjas, ninja]
-        this.setState({
-            ninjas: ninjas
-        })
-    }
-    componentDidMount(){
-     
-    }
-    componentDidUpdate(prevProps, prevState){
-        console.log(prevProps)
-        console.log(prevState)
-    }
-
-    deleteNinja = (id) =>{
-        let ninjas = this.state.ninjas.filter(ninja =>{
-            return ninja.id !== id
-        })
-        this.setState({
-            ninjas: ninjas
-        })
-
-    }
     render () {
         return (
           <div className="App">
-              <Ninjas 
-              deleteNinja={this.deleteNinja}
-              ninjas={ this.state.ninjas }/>
-              <AddNinja addNinja={this.addNinja}/>
+              hello world
           </div>
         );
     }
 }
-
 
 export default App;
