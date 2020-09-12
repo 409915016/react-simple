65d64065e513eb1b61d190bdb3b21a167c1255f4
mather <misaka@misaka.im>
2020-09-12 21:49:08 +0800

    reading list app

---
+++ src/components/BookForm.js
@@ -0,0 +1,28 @@
+import React, { useContext, useState } from 'react';
+import { BookContext } from '../contexts/BookContext';
+
+const NewBookForm = () => {
+    const { addBook } = useContext(BookContext)
+    const [title, setTitle] = useState('')
+    const [author, setAuthor] = useState('')
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        addBook(title, author)
+        setAuthor('')
+        setTitle('')
+    }
+    return ( 
+        <form onSubmit={ handleSubmit }>
+            <input type="text" placeholder="book title" 
+            onChange={ (e) => setTitle(e.target.value) } value={title} required/>
+            
+            <input type="text" placeholder="author" 
+            onChange={ (e) => setAuthor(e.target.value) } value={author} required/>
+
+            <input type="submit" value="add book"/>
+        </form>
+    );
+}
+ 
+export default NewBookForm;
